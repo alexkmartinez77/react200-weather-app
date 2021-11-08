42281faa8bd9d640cea55cb0a8e9b46221fe9dc4
Alex Martinez <alexkmartinez77@gmail.com>
2021-11-08 14:40:54 -0800

    Adds scaffold for each of 4 components and provides Header.

---
+++ src/js/CurrentWeather/CurrentWeather.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class CurrentWeather extends React.Component {
+
+  render() {
+  return (
+    <div>
+      CurrentWeather
+    </div>
+  );
+ }
+}
+
+export default CurrentWeather;
---
+++ src/js/SearchHistory/SearchHistory.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export class SearchHistory extends React.Component {
+
+  render() {
+  return (
+    <div>
+      SearchHistory
+    </div>
+  );
+ }
+}
+
+export default SearchHistory;
