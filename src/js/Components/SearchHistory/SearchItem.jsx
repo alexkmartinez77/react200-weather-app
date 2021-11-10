67b39021da419ad6c7ff43fa42afc6a0c2c3b9c3
Alex Martinez <alexkmartinez77@gmail.com>
2021-11-10 14:45:55 -0800

    Background fade.

--- src/js/Components/SearchHistory/SearchItem.jsx
+++ src/js/Components/SearchHistory/SearchItem.jsx
@@ -6,7 +6,7 @@ export class SearchItem extends React.Component {
     const { name, timeStamp: {date, time} } = this.props.data
     const index = this.props.index;
   return (
-    <div className={index % 2 == 0 ? '': 'greyBackground'}>
+    <div className={index % 2 == 0 ? '': 'blueFade'}>
       <div className={"row"}>
         <div className="col-md-8 col-lg-9 px-4 pt-3">{ name }</div>
         <div className="col-md-4 col-lg-3 pt-3">
